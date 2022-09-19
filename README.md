c985c965b466c6f9f5a6afba9106ed598336c86e
thejasvi <thejasvi.beleyur@bi.mpg.de>
2022-09-19 15:28:38 +0200

    Get Spiesberger-Wahlberg working in both C++ and Python, and perform comparison.

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ relative speed-up means larger gains for bigger matrices (e.g. 90x3), where
 instead of 0.6 s we now have 0.2s  - which could mean a lot more 
 computations over a unit time. 
 
-
 Broad results
 -------------
 Essentially the C++ (Eigen) implementation is >= 4 times faster than the 
@@ -18,3 +17,19 @@ Python (NumPy) based implementation for a simple 4-channel mic array.
 |185753 | 43919 |   4.23  |
 |187291 | 43342 |   4.32  |
 |197276 | 42374 |   4.65  |
+
+Speedups from function implementations
+--------------------------------------
+Here are the results for 1000 runs with 4-19 channels, and randomly chosen
+positions and time-differences of arrival. 
+
+The code has been run in Python using ```cppyy``` linked with Eigen and 
+my own C++ implementation. 
+
+The min-max speedup is 2.4-7.29, with the 95%ile interval being 2.68-3.65, and median
+being 2.99.
+ 
+In general - we see an overall speedup, with some very promising speedups! The slight decrease
+in speedup might be because of the data-marshalling required by switching between languages
+(deliberately assigning values of arrays between NumPy and Eigen). 
+
--- pyton_eigen.py
+++ pyton_eigen.py
@@ -19,41 +19,45 @@ except ImportError:
 from companion_python_code import python_function
 import numpy as np 
 
+all_speedups = []
+num_channels = []
+for i in range(1000):
+    
+    nmics = int(np.random.choice(np.arange(4,20), 1))
+    np_array = np.random.normal(1,10,3*(nmics)).reshape(-1,3)
+    np_d = np.random.choice(np.linspace(-0.5,0.5,100), nmics-1)
+    
+    
+    import time
+    start = time.perf_counter_ns()
+    rows, cols = np_array.shape
+    
+    array_geom = MatrixXd(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            array_geom[i,j] = np_array[i,j]
+    
+    d = VectorXd(rows-1)
+    for i, value in enumerate(np_d):
+        d[i] = value
+    
+    uu = cppyy.gbl.spiesberger_wahlberg_solution(array_geom, d, 343.0)
+    stop = time.perf_counter_ns()
+    durn_s = (stop-start)/1e9
+    #print(f'%.6f seconds' % durn_s)
+    # (0,6,0, 0,13,0, 1,0,0, 0,0,1, 2,2,4)
+    
+    start2 = time.perf_counter_ns()
+    output  = python_function(np_array, np_d)
+    stop2 = time.perf_counter_ns()
+    durn2_s = (stop2-start2)/1e9
+    
+    speedup = durn2_s/durn_s
+    #print(f'%.6f seconds'% durn2_s)
+    #print('%.2f speed up'% speedup)
+    num_channels.append(nmics)
+    all_speedups.append(speedup)
 
-np_array = np.array([0,6,0, 0,13,0, 1,0,0, 0,0,1, 2,2,4]).reshape(-1,3)
-np_d = np.array([0.01, 0.2, -0.3, 0.5])
-
-
-import time
-start = time.perf_counter_ns()
-rows, cols = np_array.shape
-
-array_geom = MatrixXd(rows, cols)
-for i in range(rows):
-    for j in range(cols):
-        array_geom[i,j] = np_array[i,j]
-
-
-d = VectorXd(rows-1)
-for i, value in enumerate(np_d):
-    d[i] = value
-
-uu = cppyy.gbl.spiesberger_wahlberg_solution(array_geom, d, 343.0)
-stop = time.perf_counter_ns()
-durn_s = (stop-start)/1e9
-print(f'%.6f seconds' % durn_s)
-# (0,6,0, 0,13,0, 1,0,0, 0,0,1, 2,2,4)
-
-start2 = time.perf_counter_ns()
-output  = python_function(np_array, np_d)
-stop2 = time.perf_counter_ns()
-durn2_s = (stop2-start2)/1e9
-
-speedup = durn2_s/durn_s
-print(f'%.6f seconds'% durn2_s)
-print('%.2f speed up'% speedup)
-
-# for i in range(np_array.shape[0])
-#     array_geom  
-
-# 
+import matplotlib.pyplot as plt 
+print(np.percentile(all_speedups, [0,2.5,50,97.5,100]))
+plt.plot(num_channels, all_speedups, '*')
